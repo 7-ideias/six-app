26856d5a82e20d0903c68524e20d000cbbcfdb57
Cartaxo <carlos.pijanowski@gmail.com>
2025-03-21 16:57:21 -0300

    atualizacao da versao do android e atualizacao do bashizinho

--- install_flutter.sh
+++ install_flutter.sh
@@ -5,13 +5,35 @@ if [ ! -d "flutter" ]; then
 fi
 
 export PATH="$PWD/flutter/bin:$PATH"
+
+echo "[LOG SIX] INICIO DO BATCH... ACOMPANHE OS LOGS"
+
+STEP=1
+TOTAL=7 #MANTENHA ISSO ATUALIZADO!
+
+log_step() {
+  echo "[LOG SIX] $(printf "%02d" $STEP) DE $(printf "%02d" $TOTAL) $1"
+  STEP=$((STEP + 1))
+}
+
+log_step "DANDO UM UPGRADE"
 flutter upgrade
+
+log_step "VERIFICANDO COM O DOCTOR COMO ESTA O FLUTTER APOS INSTALADO..."
 flutter doctor
+
+log_step "SE NAO ACHAR O ANDROID, NAO SE PREOCUPE. DEU BOM!"
+
+log_step "DANDO UM CLEANSINHO DE BOA"
 flutter clean
+
+log_step "CONFIGURANDO WEB"
 flutter config --enable-web
+
+log_step "INICIO DO PUBGET"
 flutter pub get
 #flutter gen-l10n
 
-echo "ARQUIVOS GERADOS:"
-find . -name "app_localizations.dart"
-
+echo "[LOG SIX] FINALIZEI AS CONFIGS DO PROJETO"
+#echo "ARQUIVOS GERADOS:"
+#find . -name "app_localizations.dart"
