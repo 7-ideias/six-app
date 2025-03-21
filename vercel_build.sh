5f1b2baa6d9ee51431eab2fe3c5f57c7321f2eb6
Cartaxo <carlos.pijanowski@gmail.com>
2025-03-21 13:58:00 -0300

    precisou ser ajustado o limite do comando no versel

---
+++ vercel_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git
+fi
+
+export PATH="$PWD/flutter/bin:$PATH"
+flutter upgrade
+flutter doctor
+flutter clean
+flutter config --enable-web
+flutter pub get
+flutter gen-l10n
+flutter build web --release
