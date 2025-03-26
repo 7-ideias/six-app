bef5d4a64ab87cde9ac0ef7dd0377052839f9b64
Cartaxo <carlos.pijanowski@gmail.com>
2025-03-26 08:59:21 -0300

    ajustes para fufanfar o build e logo provisoria da notificacao

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true // ✅ esta linha habilita o desugaring
     }
 
     kotlinOptions {
@@ -36,6 +37,12 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+
+    dependencies {
+        // suas dependências existentes
+
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4") // ✅ esta é a dependência necessária
     }
 }
 
