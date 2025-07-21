a0a18af948a6c945b255da397d4edee8dcf230ad
LeonardodCastro <leonardo.castro@maithatechcorp.onmicrosoft.com>
2025-07-21 10:18:55 -0300

    Arquivos que possibilitam subida rodar no iphone

---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,46 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get hello => '¡Hola!';
+
+  @override
+  String get welcome => 'Bienvenido a nuestra aplicación';
+
+  @override
+  String get preferences_title => 'Preferencias';
+
+  @override
+  String get preferences_language => 'Seleccionar Idioma';
+
+  @override
+  String get preferences_dark_mode => 'Activar Modo Oscuro';
+
+  @override
+  String get teste => 'em espanhol';
+
+  @override
+  String get dashboard_title => 'Dashboard';
+
+  @override
+  String get dashboard_web_title => 'WEB -> em espanhol';
+
+  @override
+  String get bemvindo => 'welcome';
+
+  @override
+  String get login => 'login';
+
+  @override
+  String get senha => 'password';
+
+  @override
+  String get entrar => 'entrar';
+}
---
+++ lib/l10n/app_localizations_pt.dart
@@ -0,0 +1,46 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Portuguese (`pt`).
+class AppLocalizationsPt extends AppLocalizations {
+  AppLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get hello => 'Olá!';
+
+  @override
+  String get welcome => 'Bem-vindo ao nosso app';
+
+  @override
+  String get preferences_title => 'Preferências';
+
+  @override
+  String get preferences_language => 'Selecionar Idioma';
+
+  @override
+  String get preferences_dark_mode => 'Ativar Modo Escuro';
+
+  @override
+  String get teste => 'em portugues';
+
+  @override
+  String get dashboard_title => 'Dashboard';
+
+  @override
+  String get dashboard_web_title => 'WEB -> em portugues';
+
+  @override
+  String get bemvindo => 'welcome';
+
+  @override
+  String get login => 'login';
+
+  @override
+  String get senha => 'password';
+
+  @override
+  String get entrar => 'entrar';
+}
