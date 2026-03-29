21cc677d8efbf063f2cba69d1dadd44b6c337f88
Pijam.a <carlos.pijanowski@gmail.com>
2026-03-29 13:09:15 -0300

    feature: provider de temas

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../design_system/themes/app_colors.dart';
+import '../design_system/themes/app_theme.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
+  AppPalette _currentPalette = AppPalette.corporate;
 
   ThemeMode get themeMode => _themeMode;
+  AppPalette get currentPalette => _currentPalette;
+
+  ThemeData get lightTheme => AppTheme.getTheme(_currentPalette, isDark: false);
+  ThemeData get darkTheme => AppTheme.getTheme(_currentPalette, isDark: true);
 
   ThemeProvider() {
     _loadTheme();
@@ -17,10 +24,24 @@ class ThemeProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  void setPalette(AppPalette palette) async {
+    _currentPalette = palette;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('selectedPalette', palette.index);
+    notifyListeners();
+  }
+
   Future<void> _loadTheme() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    // Carregar Dark Mode
     bool isDark = prefs.getBool('isDarkMode') ?? false;
     _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+
+    // Carregar Paleta
+    int paletteIndex = prefs.getInt('selectedPalette') ?? AppPalette.corporate.index;
+    _currentPalette = AppPalette.values[paletteIndex];
+
     notifyListeners();
   }
 }
