e0b340ed016ea9200121fea6bc6d578f68368afc
Pijam.a <carlos.pijanowski@gmail.com>
2026-04-02 21:54:52 -0300

    teste para bater no backend

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -2,7 +2,7 @@ class AppConfig {
   static const String baseUrl = String.fromEnvironment(
     'https://931c-2804-14c-ccec-8001-50b2-faa3-abe4-8605.ngrok-free.app',
     // 'API_BASE_URL',
-    // defaultValue: 'http://10.0.2.2:8082',
+    defaultValue: 'https://931c-2804-14c-ccec-8001-50b2-faa3-abe4-8605.ngrok-free.app',
     // defaultValue: 'http://localhost:8082',
   );
 }
