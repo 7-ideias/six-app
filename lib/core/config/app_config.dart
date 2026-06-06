b66a647c64d785c07a50f61f6c6c48ce66753504
= <carlos.pijanowski@gmail.com>
2026-06-06 13:34:57 -0300

    feature(20260606): teste apontando para o endpoint

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -5,5 +5,6 @@ class AppConfig {
     // defaultValue: 'http://10.0.2.2:8082',
     // defaultValue: 'http://localhost:8082',
     // defaultValue: 'https://931c-2804-14c-ccec-8001-50b2-faa3-abe4-8605.ngrok-free.app',
+    defaultValue: 'https://sixback-production.up.railway.app',
   );
 }
