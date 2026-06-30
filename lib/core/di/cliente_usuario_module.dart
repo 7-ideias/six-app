488b72b68643bb1472a27db0e532138ea6d44f49
Carlos Pijanowski Cartaxo <82827380+carlospijanowski@users.noreply.github.com>
2026-06-29 22:15:24 -0300

    refactor(cliente): adiciona modulo de dependencia

---
+++ lib/core/di/cliente_usuario_module.dart
@@ -0,0 +1,9 @@
+import '../../data/services/cliente_usuario/cliente_usuario_api_client.dart';
+import '../../domain/services/cliente_usuario/cliente_usuario_service.dart';
+
+class ClienteUsuarioModule {
+  ClienteUsuarioModule._();
+
+  static final ClienteUsuarioService clienteUsuarioService =
+      ClienteUsuarioService(apiClient: HttpClienteUsuarioApiClient());
+}
