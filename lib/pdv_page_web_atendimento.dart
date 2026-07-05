b2225d17a976b9cbc0385b94922c197e21920529
Carlos Pijanowski Cartaxo <82827380+carlospijanowski@users.noreply.github.com>
2026-07-05 14:06:36 -0300

    feat(pdv): cria tela web atendimento tecnico

---
+++ lib/pdv_page_web_atendimento.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:sixpos/presentation/screens/atendimentos_tecnicos_web_page.dart';
+
+class PDVWebAtendimento extends StatelessWidget {
+  const PDVWebAtendimento({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AtendimentosTecnicosWebPage();
+  }
+}
