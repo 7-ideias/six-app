5cbb3b06a1140fdd038aa8ca20a958696305cb24
Carlos Pijanowski Cartaxo <82827380+carlospijanowski@users.noreply.github.com>
2026-07-05 01:30:05 -0300

    feat(atendimento): adiciona api de recebimento

--- lib/data/services/atendimento_tecnico/atendimento_tecnico_api_client.dart
+++ lib/data/services/atendimento_tecnico/atendimento_tecnico_api_client.dart
@@ -8,8 +8,7 @@ import '../../models/atendimento_tecnico_models.dart';
 import '../../models/dominio_models.dart';
 
 class AtendimentoTecnicoApiClient {
-  AtendimentoTecnicoApiClient({http.Client? httpClient})
-    : _httpClient = httpClient ?? http.Client();
+  AtendimentoTecnicoApiClient({http.Client? httpClient}) : _httpClient = httpClient ?? http.Client();
 
   final http.Client _httpClient;
 
@@ -18,11 +17,7 @@ class AtendimentoTecnicoApiClient {
       Uri.parse('${AppConfig.baseUrl}/dominios/atendimento-tecnico/base'),
       headers: await _headers(),
     );
-
-    if (response.statusCode != 200) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 200) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     return AtendimentoTecnicoDominiosBaseModel.fromJson(jsonDecode(_decodeBody(response)) as Map<String, dynamic>);
   }
 
@@ -31,46 +26,29 @@ class AtendimentoTecnicoApiClient {
       Uri.parse('${AppConfig.baseUrl}/dominios/atendimento-tecnico/status/customizacoes'),
       headers: await _headers(),
     );
-
     if (response.statusCode == 204) return <DominioStatusAtendimentoCustomizacaoModel>[];
-    if (response.statusCode != 200) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 200) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     final decoded = jsonDecode(_decodeBody(response));
     if (decoded is! List) return <DominioStatusAtendimentoCustomizacaoModel>[];
     return decoded.whereType<Map<String, dynamic>>().map(DominioStatusAtendimentoCustomizacaoModel.fromJson).toList(growable: false);
   }
 
-  Future<List<DominioStatusAtendimentoCustomizacaoModel>> salvarCustomizacoesStatusAtendimento(
-    List<Map<String, dynamic>> customizacoes,
-  ) async {
+  Future<List<DominioStatusAtendimentoCustomizacaoModel>> salvarCustomizacoesStatusAtendimento(List<Map<String, dynamic>> customizacoes) async {
     final response = await _httpClient.put(
       Uri.parse('${AppConfig.baseUrl}/dominios/atendimento-tecnico/status/customizacoes'),
       headers: await _headers(),
       body: jsonEncode(customizacoes),
     );
-
-    if (response.statusCode != 200) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 200) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     final decoded = jsonDecode(_decodeBody(response));
     if (decoded is! List) return <DominioStatusAtendimentoCustomizacaoModel>[];
     return decoded.whereType<Map<String, dynamic>>().map(DominioStatusAtendimentoCustomizacaoModel.fromJson).toList(growable: false);
   }
 
   Future<List<AtendimentoTecnicoModel>> listar() async {
-    final response = await _httpClient.get(
-      Uri.parse('${AppConfig.baseUrl}/atendimentos-tecnicos'),
-      headers: await _headers(),
-    );
-
+    final response = await _httpClient.get(Uri.parse('${AppConfig.baseUrl}/atendimentos-tecnicos'), headers: await _headers());
     if (response.statusCode == 204) return <AtendimentoTecnicoModel>[];
-    if (response.statusCode != 200) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 200) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     final decoded = jsonDecode(_decodeBody(response));
     if (decoded is! List) return <AtendimentoTecnicoModel>[];
     return decoded.whereType<Map<String, dynamic>>().map(AtendimentoTecnicoModel.fromJson).toList(growable: false);
@@ -82,53 +60,37 @@ class AtendimentoTecnicoApiClient {
       headers: await _headers(),
       body: jsonEncode(input.toJson()),
     );
-
-    if (response.statusCode != 201) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 201) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     return AtendimentoTecnicoModel.fromJson(jsonDecode(_decodeBody(response)) as Map<String, dynamic>);
   }
 
-  Future<AtendimentoTecnicoModel> atualizar({
-    required String id,
-    required AtendimentoTecnicoUpdateInput input,
-  }) async {
+  Future<AtendimentoTecnicoModel> atualizar({required String id, required AtendimentoTecnicoUpdateInput input}) async {
     final response = await _httpClient.put(
       Uri.parse('${AppConfig.baseUrl}/atendimentos-tecnicos/' + id),
       headers: await _headers(),
       body: jsonEncode(input.toJson()),
     );
-
-    if (response.statusCode != 200) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 200) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     return AtendimentoTecnicoModel.fromJson(jsonDecode(_decodeBody(response)) as Map<String, dynamic>);
   }
 
-  Future<AtendimentoTecnicoModel> alterarStatus({
-    required String id,
-    required int statusId,
-    required String statusCodigo,
-    required String statusI18nKey,
-    String? observacao,
-  }) async {
+  Future<AtendimentoTecnicoModel> receber({required String id, required AtendimentoTecnicoRecebimentoInput input}) async {
+    final response = await _httpClient.post(
+      Uri.parse('${AppConfig.baseUrl}/atendimentos-tecnicos/' + id + '/recebimentos'),
+      headers: await _headers(),
+      body: jsonEncode(input.toJson()),
+    );
+    if (response.statusCode != 200) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
+    return AtendimentoTecnicoModel.fromJson(jsonDecode(_decodeBody(response)) as Map<String, dynamic>);
+  }
+
+  Future<AtendimentoTecnicoModel> alterarStatus({required String id, required int statusId, required String statusCodigo, required String statusI18nKey, String? observacao}) async {
     final response = await _httpClient.patch(
       Uri.parse('${AppConfig.baseUrl}/atendimentos-tecnicos/' + id + '/status'),
       headers: await _headers(),
-      body: jsonEncode(<String, dynamic>{
-        'statusId': statusId,
-        'statusCodigo': statusCodigo,
-        'statusI18nKey': statusI18nKey,
-        'observacao': observacao,
-      }),
+      body: jsonEncode(<String, dynamic>{'statusId': statusId, 'statusCodigo': statusCodigo, 'statusI18nKey': statusI18nKey, 'observacao': observacao}),
     );
-
-    if (response.statusCode != 200) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 200) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     return AtendimentoTecnicoModel.fromJson(jsonDecode(_decodeBody(response)) as Map<String, dynamic>);
   }
 
@@ -138,54 +100,24 @@ class AtendimentoTecnicoApiClient {
       headers: await _headers(),
       body: jsonEncode(<String, dynamic>{'baseUrl': baseUrl, 'validadeMinutos': 1440}),
     );
-
-    if (response.statusCode != 201) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 201) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     return jsonDecode(_decodeBody(response)) as Map<String, dynamic>;
   }
 
   Future<Map<String, dynamic>> consultarAssinaturaPublica({required String idUnicoDaEmpresa, required String token}) async {
-    final uri = Uri.parse('${AppConfig.baseUrl}/public/api/atendimentos-tecnicos/assinatura').replace(queryParameters: <String, String>{
-      'idUnicoDaEmpresa': idUnicoDaEmpresa,
-      'token': token,
-    });
+    final uri = Uri.parse('${AppConfig.baseUrl}/public/api/atendimentos-tecnicos/assinatura').replace(queryParameters: <String, String>{'idUnicoDaEmpresa': idUnicoDaEmpresa, 'token': token});
     final response = await _httpClient.get(uri, headers: _publicHeaders());
-
-    if (response.statusCode != 200) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 200) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     return jsonDecode(_decodeBody(response)) as Map<String, dynamic>;
   }
 
-  Future<Map<String, dynamic>> aprovarAssinaturaPublica({
-    required String idUnicoDaEmpresa,
-    required String token,
-    required String nomeAssinante,
-    required String? documentoAssinante,
-    required String assinaturaDataUrl,
-    required String? observacao,
-  }) async {
+  Future<Map<String, dynamic>> aprovarAssinaturaPublica({required String idUnicoDaEmpresa, required String token, required String nomeAssinante, required String? documentoAssinante, required String assinaturaDataUrl, required String? observacao}) async {
     final response = await _httpClient.post(
       Uri.parse('${AppConfig.baseUrl}/public/api/atendimentos-tecnicos/assinatura/aprovar'),
       headers: _publicHeaders(),
-      body: jsonEncode(<String, dynamic>{
-        'idUnicoDaEmpresa': idUnicoDaEmpresa,
-        'token': token,
-        'nomeAssinante': nomeAssinante,
-        'documentoAssinante': documentoAssinante,
-        'assinaturaDataUrl': assinaturaDataUrl,
-        'aceitouTermos': true,
-        'observacao': observacao,
-      }),
+      body: jsonEncode(<String, dynamic>{'idUnicoDaEmpresa': idUnicoDaEmpresa, 'token': token, 'nomeAssinante': nomeAssinante, 'documentoAssinante': documentoAssinante, 'assinaturaDataUrl': assinaturaDataUrl, 'aceitouTermos': true, 'observacao': observacao}),
     );
-
-    if (response.statusCode != 201) {
-      throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
-    }
-
+    if (response.statusCode != 201) throw AtendimentoTecnicoApiException(statusCode: response.statusCode, body: _decodeBody(response));
     return jsonDecode(_decodeBody(response)) as Map<String, dynamic>;
   }
 
@@ -193,25 +125,12 @@ class AtendimentoTecnicoApiClient {
     final authService = AuthService();
     final jwtToken = await authService.getAccessToken();
     final idUnicoDaEmpresa = await authService.getEmpresaId();
-
-    return <String, String>{
-      'idUnicoDaEmpresa': idUnicoDaEmpresa ?? '',
-      'Content-Type': 'application/json; charset=utf-8',
-      'Accept': 'application/json; charset=utf-8',
-      'Authorization': 'Bearer ' + (jwtToken ?? ''),
-    };
+    return <String, String>{'idUnicoDaEmpresa': idUnicoDaEmpresa ?? '', 'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + (jwtToken ?? '')};
   }
 
-  Map<String, String> _publicHeaders() {
-    return const <String, String>{
-      'Content-Type': 'application/json; charset=utf-8',
-      'Accept': 'application/json; charset=utf-8',
-    };
-  }
+  Map<String, String> _publicHeaders() => const <String, String>{'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json; charset=utf-8'};
 
-  String _decodeBody(http.Response response) {
-    return utf8.decode(response.bodyBytes);
-  }
+  String _decodeBody(http.Response response) => utf8.decode(response.bodyBytes);
 }
 
 class AtendimentoTecnicoApiException implements Exception {
@@ -221,7 +140,5 @@ class AtendimentoTecnicoApiException implements Exception {
   final String body;
 
   @override
-  String toString() {
-    return 'AtendimentoTecnicoApiException(statusCode: $statusCode, body: $body)';
-  }
+  String toString() => 'AtendimentoTecnicoApiException(statusCode: $statusCode, body: $body)';
 }
