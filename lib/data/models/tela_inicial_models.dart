24740518c89599c0a5a710966801645173c95c26
Carlos Pijanowski Cartaxo <82827380+carlospijanowski@users.noreply.github.com>
2026-07-07 00:15:35 -0300

    feat(mobile): adiciona indicadores de atendimento tecnico no modelo inicial

--- lib/data/models/tela_inicial_models.dart
+++ lib/data/models/tela_inicial_models.dart
@@ -1,15 +1,29 @@
 class TelaInicialModel {
   final int totalVendasAbertas;
+  final int totalAtendimentoTecnicosNaoEntregues;
+  final int totalAtendimentoTecnicoEmAndamento;
+  final int totalAtendimentoTecnicoAguardandoAssinatura;
   final int totalOrdensDeServicoAbertas;
 
   TelaInicialModel({
     required this.totalVendasAbertas,
+    required this.totalAtendimentoTecnicosNaoEntregues,
+    required this.totalAtendimentoTecnicoEmAndamento,
+    required this.totalAtendimentoTecnicoAguardandoAssinatura,
     required this.totalOrdensDeServicoAbertas,
   });
 
   factory TelaInicialModel.fromJson(Map<String, dynamic> json) {
     return TelaInicialModel(
       totalVendasAbertas: json['totalVendasAbertas'] ?? 0,
+      totalAtendimentoTecnicosNaoEntregues:
+          json['totalAtendimentoTecnicosNaoEntregues'] ?? 0,
+      totalAtendimentoTecnicoEmAndamento:
+          json['totalAtendimentoTecnicoEmAndamento'] ?? 0,
+      totalAtendimentoTecnicoAguardandoAssinatura:
+          json['totalAtendimentoTecnicoAguardandoAssinatura'] ??
+          json['totalAntedimentoTecnicoAguardandoAssinatura'] ??
+          0,
       totalOrdensDeServicoAbertas: json['totalOrdensDeServicoAbertas'] ?? 0,
     );
   }
@@ -17,8 +31,12 @@ class TelaInicialModel {
   Map<String, dynamic> toJson() {
     return {
       'totalVendasAbertas': totalVendasAbertas,
+      'totalAtendimentoTecnicosNaoEntregues':
+          totalAtendimentoTecnicosNaoEntregues,
+      'totalAtendimentoTecnicoEmAndamento': totalAtendimentoTecnicoEmAndamento,
+      'totalAtendimentoTecnicoAguardandoAssinatura':
+          totalAtendimentoTecnicoAguardandoAssinatura,
       'totalOrdensDeServicoAbertas': totalOrdensDeServicoAbertas,
     };
   }
 }
-
