a82d1b8c7a0a3ffcfd26ef57a8ce037f21d7f5cc
Carlos Pijanowski Cartaxo <82827380+carlospijanowski@users.noreply.github.com>
2026-07-11 19:24:17 -0300

    feat(produto): adiciona Lottie full-screen no carregamento mobile

--- lib/core/constants/six_animation_assets.dart
+++ lib/core/constants/six_animation_assets.dart
@@ -3,4 +3,7 @@ class SixAnimationAssets {
 
   static const String saleProcessing =
       'assets/animations/sale_processing.json';
+
+  static const String productCatalogLoading =
+      'assets/animations/product_boxes_loading.json';
 }
