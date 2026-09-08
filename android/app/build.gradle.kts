686b8aaa7cf08cb46a5a12343e55d7c4659c8667
Carlos Pijanowski Cartaxo <82827380+carlospijanowski@users.noreply.github.com>
2026-09-08 07:31:53 -0700

    build(android): ajusta minSdk para biometria moderna

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,9 +26,8 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "br.com.seteideias.appplanilha"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // local_auth 3.x suporta Android API 24+.
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
